fca33d1ddc55190a21fc02ab921facfe8cbcce8b
Sarvesh P <sarveshjaiofficial@gmail.com>
2025-02-11 11:51:16 +0530

    latest

--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -1,16 +1,18 @@
+
 declare module "next-auth" {
-    interface Session {
-      accessToken: string;
-      refreshToken: string;
-      accessTokenIssuedAt: number;
-      accessTokenExpiresAt: number;
-    }
-  
-    interface JWT {
-      access_token: string;
-      refresh_token: string;
-      issued_at: number;
-      expires_at: number;
-      error?: string; // Handle refresh token errors
-    }
+  interface Session {
+    accessToken?: string;
+    refreshToken?: string;
+    accessTokenIssuedAt?: number;
+    accessTokenExpiresAt?: number;
+    error?: string; // Handle refresh token errors
   }
+
+  interface JWT {
+    access_token?: string;
+    refresh_token?: string;
+    issued_at?: number;
+    expires_at?: number;
+    error?: string; // Handle refresh token errors
+  }
+}
