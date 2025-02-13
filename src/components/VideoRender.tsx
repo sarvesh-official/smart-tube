9666d1728b8fd263f69c182673f03c641cb012bc
Sarvesh P <sarveshjaiofficial@gmail.com>
2025-02-13 14:29:12 +0530

    latest

--- src/components/VideoRender.tsx
+++ src/components/VideoRender.tsx
@@ -5,6 +5,7 @@ import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 import DescriptionBox from "./DescriptionBox";
 import VideoSkeleton from "./VideoSkeleton";
+import PlaylistContents from "./PlaylistContents";
 
 const VideoRender = ({
   isOpen,
@@ -17,7 +18,7 @@ const VideoRender = ({
   const { watchVideo, fetchVideo, loading } = useYoutube();
 
   useEffect(() => {
-    if (session && videoId  ) {
+    if (session && videoId) {
       fetchVideo(session, videoId);
     }
   }, [session, videoId, fetchVideo]);
@@ -27,26 +28,34 @@ const VideoRender = ({
   }
 
   return (
-    <div className={`overflow-y-scroll custom-scrollbar ${
-          isOpen ? "w-4/5" : "w-screen"
-        } pt-6 p-4`}>
+    <div
+      className={`overflow-y-scroll custom-scrollbar ${
+        isOpen ? "w-4/5" : "w-screen"
+      } pt-6 p-4`}
+    >
       <div>
         {watchVideo ? (
-          <div className="text-white w-full">
+          <div className="text-white w-full flex gap-6">
             <div className="rounded-xl w-3/5">
               <iframe
-              className={`rounded-xl ${ isOpen ? "w-[560px] h-[350px]" : "w-[683px] h-[400px]"}`}
-             
+                className={`rounded-xl ${
+                  isOpen ? "w-[560px] h-[350px]" : "w-[683px] h-[400px]"
+                }`}
                 src={`https://www.youtube.com/embed/${watchVideo.videoId}`}
                 title="YouTube video player"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerPolicy="strict-origin-when-cross-origin"
                 allowFullScreen
               ></iframe>
-            <h1 className="font-semibold text-xl w-auto my-2">{watchVideo.title}</h1>
-            {watchVideo.description &&
-             <DescriptionBox description={watchVideo.description}/>
-            }
+              <h1 className="font-semibold text-xl w-auto my-2">
+                {watchVideo.title}
+              </h1>
+              {watchVideo.description && (
+                <DescriptionBox description={watchVideo.description} />
+              )}
+            </div>
+            <div>
+              <PlaylistContents />
             </div>
           </div>
         ) : (
@@ -55,8 +64,6 @@ const VideoRender = ({
           </div>
         )}
       </div>
-
-      <div></div>
     </div>
   );
 };
